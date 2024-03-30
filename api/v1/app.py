69156f374c7726184c0c29a980e8eb35899885ef
oumaima <oumaimanaanaapro@gmail.com>
2024-03-30 14:18:20 +0000

    task3 by ouma

---
+++ api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" This script creates an api flask for hbnb project"""
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(exception=None):
+    return storage.close()
+
+
+if __name__ == "__main__":
+    if "HBNB_API_HOST" in os.environ:
+        host = os.environ.get("HBNB_API_HOST")
+    else:
+        host = "0.0.0.0"
+
+    if "HBNB_API_PORT" in os.environ:
+        port = os.environ.get("HBNB_API_PORT")
+    else:
+        port = "5000"
+
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This script creates a route /status on the object app_views """
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    return jsonify({"status": "OK"})
