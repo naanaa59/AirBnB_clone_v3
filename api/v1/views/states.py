cc67f353d454c99ee51d634ed6652c44b557d36a
oumaima <oumaimanaanaapro@gmail.com>
2024-03-31 14:49:15 +0000

    task6

---
+++ api/v1/views/states.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+""" This script handles all default RESTFul API actions """
+
+from models.state import State
+from flask import jsonify, request
+from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def states():
+    all_states = storage.all(State).values()
+    state_list = []
+    for state in all_states:
+        state_list.append(state.to_dict())
+    return jsonify(state_list)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def state_id_get(state_id):
+    state_obj = storage.get(State, state_id)
+    if not state_obj:
+        abort(404)
+
+    return jsonify(state_obj.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_
+                 slashes=False)
+def state_id_delete(state_id):
+    state_obj = storage.get(State, state_id)
+    if not state_obj:
+        abort(404)
+    storage.delete(state_obj)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def state_post():
+    state_obj_dict = request.get_json()
+
+    if not state_obj_dict:
+        abort(400, "Not a JSON")
+
+    if 'name' not in state_obj_dict:
+        abort(400, "Missing name")
+
+    state_inst = State(**state_obj_dict)
+    state_inst.save()
+
+    return jsonify(state_inst.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def state_put(state_id):
+
+    state_obj = storage.get(State, state_id)
+
+    if not state_obj:
+        abort(404)
+
+    data = request.get_json()
+
+    if not data:
+        abort(400, "Not a JSON")
+
+    for key, value in data.items():
+        if key == "id" or key == "created_at" or key == "updated_at":
+            pass
+        setattr(state_obj, key, value)
+        storage.save()
+
+    return jsonify(state_obj.to_dict()), 201
